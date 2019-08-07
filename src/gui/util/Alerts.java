fe1d483226f85805518ded27d93d6cc46bcb2eb9
raysilent7 <rgc471@hotmail.com>
2019-08-07 17:20:06 -0300

    About.fxml, methods onAboutAcation and loadView

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -5,8 +5,7 @@ import javafx.scene.control.Alert.AlertType;
 
 public class Alerts {
 
-    public static void showAlert (String title, String header, String content, AlertType type) {
-
+    public static void showAlert(String title, String header, String content, AlertType type) {
         Alert alert = new Alert(type);
         alert.setTitle(title);
         alert.setHeaderText(header);
--- src/gui/util/Constraints.java
+++ src/gui/util/Constraints.java
@@ -3,6 +3,7 @@ package gui.util;
 import javafx.scene.control.TextField;
 
 public class Constraints {
+
     public static void setTextFieldInteger(TextField txt) {
         txt.textProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue != null && !newValue.matches("\\d*")) {
